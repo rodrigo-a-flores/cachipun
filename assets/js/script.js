f572f9b54c9079a48bf67782f9b80369d5488faf
Rodrigo A. Flores <149648630+rodrigo-a-flores@users.noreply.github.com>
2024-02-06 21:10:05 -0300

    Proyecto generado y finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+let numJuegos = parseInt(prompt('¿Cuantas veces quiere jugar?'));
+for(let i=0;i<numJuegos;i++){
+    let player = prompt('Escoge: piedra, papel o tijera');
+    let computer = Math.floor(Math.random()*3);
+    switch(computer){
+        case 0:
+            computer = 'piedra';
+            console.log(computer);
+            break;
+        case 1:
+            computer = 'papel';
+            console.log(computer);
+            break;
+        case 2:
+            computer = 'tijera';
+            console.log(computer);
+            break;
+        default:
+            alert('Opcion indeterminada');
+    }
+
+    // OPCION: JUGADOR ESCOGE PIEDRA
+    if(player == 'piedra' && computer == 'piedra'){
+        alert('Es un empate.');
+    }else if(player == 'piedra' && computer == 'papel'){
+        alert('Has perdido contra la maquina');
+    }else if(player == 'piedra' && computer == 'tijera'){
+        alert('Has ganado, felicitaciones.');
+    }
+
+    // OPCION: JUGADOR ESCOGE PAPEL
+    if(player == 'papel' && computer == 'piedra'){
+        alert('Has ganado, felicitaciones.');
+    }else if(player == 'papel' && computer == 'papel'){
+        alert('Es un empate.');
+    }else if(player == 'papel' && computer == 'tijera'){
+        alert('Has perdido contra la maquina');
+    }
+
+    // OPCION: JUGADOR ESCOGE TIJERA
+    if(player == 'tijera' && computer == 'piedra'){
+        alert('Has perdido contra la maquina');
+    }else if(player == 'tijera' && computer == 'papel'){
+        alert('Has ganado, felicitaciones.');
+    }else if(player == 'tijera' && computer == 'tijera'){
+        alert('Es un empate.');
+    }
+
+    // OPCION: JUGADOR ESCOGE UNA OPCION INVALIDA
+    if(player != 'piedra' && player != 'papel' && player != 'tijera'){
+        alert('Opción inválida.');
+    }
+}
